181c8da7c08a5f387e0dfbce29cb16c899903caf
Pankaj Sharma <pankaj@cmdshift.net>
2021-07-15 12:13:23 +0000

    Update config/deploy.sh

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,7 +4,7 @@ cd /opt/apps/gofit/
 git pull origin master
 source /home/admin/.virtualenvs/gofit/bin/activate
 pip install -r /opt/apps/gofit/requirements.txt
-/home/admin/.virtualenvs/gofit/bin/python manage.py migrate --shared
+/home/admin/.virtualenvs/gofit/bin/python manage.py migrate 
 /home/admin/.virtualenvs/gofit/bin/python manage.py collectstatic --noinput
 sudo supervisorctl stop gofit
 kill $(lsof -t -i:8016)
