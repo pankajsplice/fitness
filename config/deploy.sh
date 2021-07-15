3a4ca0edf9cf76637f854e8000d423f4dc822474
Pankaj Sharma <pankaj@cmdshift.net>
2021-07-15 12:08:27 +0000

    Update config/nginx/gofit, config/supervisor/gofit.conf, config/production/settings_local.py, config/deploy.sh files Deleted config/settings_local.py

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd /opt/apps/ducis_api/
+cd /opt/apps/gofit/
 git pull origin master
-source /home/admin/.virtualenvs/ducis_api/bin/activate
-pip install -r /opt/apps/ducis_api/requirements.txt
-/home/admin/.virtualenvs/ducis_api/bin/python manage.py migrate_schemas --shared
-/home/admin/.virtualenvs/ducis_api/bin/python manage.py collectstatic --noinput
-sudo supervisorctl stop ducis_api
-kill $(lsof -t -i:8000)
-sudo supervisorctl start ducis_api
+source /home/admin/.virtualenvs/gofit/bin/activate
+pip install -r /opt/apps/gofit/requirements.txt
+/home/admin/.virtualenvs/gofit/bin/python manage.py migrate --shared
+/home/admin/.virtualenvs/gofit/bin/python manage.py collectstatic --noinput
+sudo supervisorctl stop gofit
+kill $(lsof -t -i:8016)
+sudo supervisorctl start gofit
--- config/production/settings_local.py
+++ config/production/settings_local.py
@@ -1,13 +1,17 @@
 # # Database
 # # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 #
+import pymysql
+pymysql.install_as_MySQLdb()
+ALLOWED_HOSTS = ['gofit.keycorp.in']
+DEBUG = True
 DATABASES = {
     'default': {
-        'ENGINE': 'tenant_schemas.postgresql_backend',
-        'NAME': 'ducisdb',
-        'USER': 'postgres',
-        'PASSWORD': '5VFonntogRuVvp4mvWC1',
-        'HOST': 'ducisdb.cur4wfqjcdam.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gofit',
+        'USER': 'admin',
+        'PASSWORD': '@dm!9098)(*',
+        'HOST': '209.126.86.200',
+        'PORT': '',
     }
 }
